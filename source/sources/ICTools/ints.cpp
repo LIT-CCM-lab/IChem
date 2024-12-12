322f19b87efadfd042e43d955cad425b128952f7
Nawfal Eddaoudi <neddaoudi@unistra.fr>
2024-12-12 12:48:37 +0100

    up

--- source/sources/ICTools/ints.cpp
+++ source/sources/ICTools/ints.cpp
@@ -31,8 +31,8 @@ void IChemSwitch::helpints()      const
            << "      -D_Hyd   N (4.5)  Hydrophobic length                      (Angstroem)"<<endl
            << "      -D_Io    N (4.0)  Ionic length                            (Angstroem)"<<endl
            << "      -D_Me    N (2.8)  Metal/Acceptor length                   (Angstroem)"<<endl
-           << "      -D_Ar    N (4.0)  Aromatic interaction length             (Angstroem)"<<endl
-           << "      -D_Pic   N (4.0)  Pi cation interaction length            (Angstroem)"      <<endl
+           << "      -D_Ar    N (5.0)  Aromatic interaction length             (Angstroem)"<<endl
+           << "      -D_Pic   N (5.0)  Pi cation interaction length            (Angstroem)"      <<endl
            << "      -a_H     N (Pi)   HBond angle                             (rad)"	   <<endl
            << "      -at_H    N (Pi/3) HBond tolerance angle                   (rad)"    <<endl
            << "      -a_ArFF  N (Pi)   Aromatic Face to Face interaction angle (rad)"       <<endl
