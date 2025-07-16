9349c16074bc17cfaa9c69137c3fb3e7c6fd1076
Nawfal Eddaoudi <neddaoudi@unistra.fr>
2025-07-16 17:55:11 +0200

    up

--- source/headers/ICCalcs/interaction.h
+++ source/headers/ICCalcs/interaction.h
@@ -145,31 +145,6 @@ namespace ICMole {
       void checkAromaticHydrophobicInteractions(Molecule& ligand, Molecule& protein, InterResults& interResult, int& NInter, double dist_H, double Dist_H) const;
       void processAromaticInteractions(Molecule& ligand, NeighborSearch& neighborSearch, const std::vector<Atom*>& proteinAtoms, double max_allowed_dist, InterResults& interResult, const bool* wInterType, int& NInter, double min_allowed_dist) const;
 
-      // void processAromaticInteractions(
-      //   Molecule &ligand,
-      //   Grid &grid,
-      //   double max_allowed_dist,
-      //   InterResults &interResult,
-      //   const bool *wInterType,
-      //   int &NInter,
-      //   double min_allowed_dist,    
-      //   // double Dist_PiCation,       
-      //   // double Angl_PiCation,
-      //   // double AngT_PiCation,       
-      //   // double Dist_Arom,           
-      //   // double dist_Arom,          
-      //   // double Angl_AromFF,        
-      //   // double AngT_AromFF,         
-      //   // double Angl_AromEF,        
-      //   // double AngT_AromEF,         
-      //   // double Dist_H,              
-      //   // double dist_H,              
-      //   double &dist,              
-      //   std::map<Residu*, resbest> &hydlist
-      // ) const;
-
-
-
       void addInteraction(InterResults& interResult, Atom& atomP, Atom& atomL, double dist, int& NInter, double* angle, unsigned int interactionType) const;
       void checkMetalNitrogenSulfonamideCase(Atom& atomL, Atom& atomP, double dist, InterResults& interResult, int& NInter) const;
       void checkMetalInteractions(Atom& atomL, Atom& atomP, double dist, InterResults& interResult, int& NInter) const;
--- source/sources/ICCalcs/interaction.cpp
+++ source/sources/ICCalcs/interaction.cpp
@@ -487,8 +487,7 @@ void Interactions::processAromaticInteractions(Molecule& ligand, NeighborSearch&
             Atom& prot = *proteinAtoms[h.protein_idx];
             double dist = std::sqrt(h.distance_squared);
 
-            // Make sure you don't compare the ligand to itself: Identifier before was incorrect in the sense that both identifiers (from atom ligand cycle and protein atom) can be the same but they are not from same molecule (ligand)
-            // Using references here prevent us from comparing the ligand to itself
+            // Not the same molecule
             if (&prot.getParent() == &ligand)
                 continue;
 
@@ -536,7 +535,7 @@ void Interactions::processAromaticInteractions(Molecule& ligand, NeighborSearch&
                 
                 const Coords& nL = ligCycle.getNormVector(i);
 
-                for (size_t j = 0;j < cycleP.getNumAtom(); ++j) {
+                for (size_t j = 0; j < cycleP.getNumAtom(); ++j) {
                     
                     Atom& aP = *cycleP.getAtom(j);
                     if (!aP.isCarbon()) 
@@ -900,138 +899,8 @@ void Interactions::mergeSpeInts(InterResults& interResult)const
 
 }
 
-// std::string Interactions::toString(const  InterResults& interResult)const
-// {
-//     ostringstream oss;
 
-//     for (std::vector<InterPoint>::const_iterator it=interResult.listInters.begin();
-//          it != interResult.listInters.end();it++)
-//     {
-//         const InterPoint& interpt=*it;
-//         if (interpt.merged_to != -1)continue;
-//         switch (interpt.interaction)
-//         {
-//         case InterType::HBOND_LIG:   oss << "HBond_LIG         ";break;
-//         case InterType::HBOND_PROT:  oss << "HBond_PROT        ";break;
-//         case InterType::IONIC_LIG:   oss << "Ionic_LIG         ";break;
-//         case InterType::IONIC_PROT:  oss << "Ionic_PROT        ";break;
-//         case InterType::METAL:       oss << "Metal_Acceptor    ";break;
-//         case InterType::HYDROPHOBIC: oss << "Hydrophobic       ";break;
-//         case InterType::ARFACEFACE:  oss << "Aromatic_Face/Face";break;
-//         case InterType::AREDGEFACE:  oss << "Aromatic_Edge/Face";break;
-//         case InterType::PICATION:    oss << "Pi/Cation         ";break;
-//         case InterType::WHBOND_LIG:  oss << "Weak_HBond_LIG    ";break;
-//         case InterType::WHBOND_PROT: oss << "Weak_HBond_PROT   ";break;
-//         }
-// //        if interpt.interaction != InterType::PICATION
-//         if (interpt.interaction != InterType::AREDGEFACE
-//                 &&interpt.interaction != InterType::ARFACEFACE
-//                 &&interpt.interaction != InterType::PICATION)
-//         {
-//             oss << "\t"; oss.setf(ios::left);
-//             oss << "|" << interpt.Prot_Ref->getIdentifier();
-//             oss << "\t|" << interpt.Lig_Ref->getIdentifier();
-//             oss << "\t|" << interpt.point;
-//             oss << "\t|"; oss.width(7); oss.setf(ios::right); oss<< interpt.dist;
-//             oss << "\t|";
-//             if (interpt.angle != -100000)
-//                 oss <<(interpt.angle*180/M_PI);
-//             else oss<<"/";
-//         }
-//         else if (interpt.interaction== InterType::PICATION)
-//         {
-//             if (interpt.Prot_Ref->getName()=="DuAr")
-//             {
-//                 Cycle* cyc=interpt.Prot_Ref->getParent().getCycleFromCenter(interpt.Prot_Ref);
-
-//                 oss << "\t"; oss.setf(ios::left);
-//                 oss << "|"; oss.width(5);oss.setf(ios::left); oss<< cyc->getCenter().getName();
-//                 oss << "|"; oss.width(5);oss.setf(ios::left); oss << "NULL";
-//                 oss << "|"; oss << cyc->getAtom(0)->getResidu()->getIdentifier();
-
-//                 oss << "\t|" << interpt.Lig_Ref->getIdentifier();
-//                 oss << "\t|" << interpt.point;
-//                 oss << "\t|"; oss.width(7); oss.setf(ios::right); oss << interpt.dist;
-//                 oss << "\t|";
-//                 if (interpt.angle != -100000)
-//                     oss <<(interpt.angle*180/M_PI);
-//                 else oss<<"/";
-
-
-// //                oss<<"|";oss.width(7);oss<<"|";
-// //                oss.width(5);oss.setf(ios::left); oss<< cyc->getCenter().getName();
-// //                oss<<"|";
-// //                oss.width(5); oss<< "NULL";
-// //                oss <<"|";oss.width(9);oss<<cyc->getAtom(0)->getResidu()->getIdentifier();
-// //                oss<< "|";oss.width(7);oss
-// //                   <<interpt.Lig_Ref->getIdentifier();
-// //                oss<< "|";oss.width(7);oss<<interpt.dist
-// //                 <<oss.width(7);
-// //                if (interpt.angle != -100000)
-// //                    oss   <<(interpt.angle*180/M_PI);
-// //                else oss<<"/";
-//             }
-//             else
-//             {
-//                 oss << "\t"; oss.setf(ios::left);
-//                 oss << "|" << interpt.Prot_Ref->getIdentifier();
-//                 Cycle* cyc = interpt.Lig_Ref->getParent().getCycleFromCenter(interpt.Lig_Ref);
-
-//                 oss << "\t";
-//                 oss << "|"; oss.setf(ios::left, ios::adjustfield); oss.width(5); oss << cyc->getCenter().getName(); // Without adjustfield, it does not work
-//                 oss << "|"; oss.setf(ios::left); oss.width(5); oss << "0";
-//                 oss << "|"; oss << "UNK1001-XX";
-//                 oss << "\t|" << interpt.point;
-//                 oss << "\t|"; oss.width(7); oss.setf(ios::right); oss<< interpt.dist;
-//                 oss << "\t|";
-//                 oss <<(interpt.angle*180/M_PI);
-
-//             }
-//         }
-//         else
-//         {   
-//             // here
-//             Cycle* cyc=interpt.Prot_Ref->getParent().getCycleFromCenter(interpt.Prot_Ref);
-//             oss << "\t";
-//             oss << "|"; oss.setf(ios::left, ios::adjustfield); oss.width(5); oss << cyc->getCenter().getName(); // Without adjustfield, it does not work
-//             oss << "|"; oss.setf(ios::left); oss.width(5); oss << "NULL";
-//             oss << "|"; oss << cyc->getAtom(0)->getResidu()->getIdentifier();
-
-//             cyc=interpt.Lig_Ref->getParent().getCycleFromCenter(interpt.Lig_Ref);
-
-//             oss << "\t";
-//             oss << "|"; oss.setf(ios::left, ios::adjustfield); oss.width(5); oss << cyc->getCenter().getName(); // Without adjustfield, it does not work
-//             oss << "|"; oss.setf(ios::left); oss.width(5); oss << "NULL";
-//             oss << "|"; oss << cyc->getAtom(0)->getResidu()->getIdentifier();
-
-//             oss << "\t|" << interpt.point;
-//             oss << "\t|"; oss.width(7); oss.setf(ios::right); oss<< interpt.dist;
-//             oss << "\t|/";
-
-//         }
-//         oss <<endl;
-// //        for (std::vector<InterPoint>::const_iterator it2=interResult.listInters.begin();
-// //             it2 != interResult.listInters.end();it2++)
-// //        {
-// //            const InterPoint& interpt2=*it2;
-// //            if (interpt2.merged_to!= interpt.point) continue;
-// //            oss<<"\t|-->"<<interpt2.Prot_Ref->getIdentifier()
-// //              <<"\t"<<interpt2.Lig_Ref->getIdentifier()
-// //             <<"\t"<<interpt2.dist
-// //            <<"\t";
-// //            if (interpt2.angle != -100000)
-// //                oss   <<(interpt2.angle*180/M_PI);
-// //            else oss<<"/";
-// //            oss<<"\n";
-// //        }
-//     }
-//     return oss.str();
-
-// }
-
-
-std::string Interactions::toString(const InterResults& interResult) const
-{
+std::string Interactions::toString(const InterResults& interResult) const {
     ostringstream oss;
 
     // Stores ligand cycle atoms per interaction index
@@ -1157,9 +1026,6 @@ std::string Interactions::toString(const InterResults& interResult) const
     }
     return oss.str();
 }
-
-
-
 
 
 void Interactions::interToMOL2(  InterResults& interResult,
